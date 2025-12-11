aa63f662a28fe6844b15d453c10817a0d396df3f
code0110 <cod.binar@gmail.com>
2025-12-11 13:21:32 +0200

    setup api routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    // Dacă vrei doar să nu mai dea eroare și folosești SPA separat:
+    return response()->json([
+        'message' => 'Te rugăm să te autentifici prin interfața SPA / front-end.',
+    ], 401);
+})->name('login');

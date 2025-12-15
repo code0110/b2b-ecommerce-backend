5edc7516a3a115e91dabeda5554736672c236075
code0110 <cod.binar@gmail.com>
2025-12-15 11:50:01 +0200

    Create 2025_12_15_094116_add_price_to_products_table.php

---
+++ database/migrations/2025_12_15_094116_add_price_to_products_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('price', 12, 2)->default(0)->after('name');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('price');
+        });
+    }
+};
